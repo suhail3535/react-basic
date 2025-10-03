69f3118d9444e9e7529beda29f3e19dcbf24d4aa
suhail khan <khansohail015@gmail.com>
2025-10-03 21:48:24 +0530

    how to use image

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -2,10 +2,12 @@
 import "./App.css"
 import Counter from "./components/Counter"
 import Toggler from "./components/Toggler"
+import Welcome from "./components/Welcome"
 
 function App() {
   return (
     <div>
+    <Welcome/>
    <Counter/>
    <Toggler/>
     </div>
--- first-react-app/src/components/Welcome.jsx
+++ first-react-app/src/components/Welcome.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
+import suhail from "../assets/images/profile.jpg"
 const Welcome = ({ school="DPS", course="LKG"}) => {
   return (
     <div>
         <h1>Welcome to {school} and class is {course}.</h1>
+        <img style={{width:'100px',height:'100px'}} src={suhail} alt='profile' />
+        <img style={{width:'100px',height:'100px'}} src='https://media.geeksforgeeks.org/gfg-gg-logo.svg' alt='logo' />
     </div>
   )
 }
